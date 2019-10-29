932a7ff63879c4f7cec6f2b402d38b310e755b32
Eli <eyturner@umich.edu>
2019-10-29 00:26:39 -0700

    Cleaning up code

--- src/Border_Analytics.py
+++ src/Border_Analytics.py
@@ -9,25 +9,27 @@ def main():
     outputFile = argv[2]
 
     importantValues = []
-    startCol = 3
-    with open(inputFile) as file:
+    with open(csvFile) as file:
         reader = csv.reader(file, delimiter=",")
         lineCount = 0
         for row in list(reader):
-            importantValues.append(row[startCol:])
+            if(lineCount == 0):
+                borderIndex = row.index('Border')
+                valueIndex = row.index('Value')
+            importantValues.append(row[borderIndex:valueIndex + 1])
             lineCount+= 1
 
+    depth = valueIndex - borderIndex
     outputDict = {}
     endList = []
     dataTypes = importantValues[0]
-    depth = len(row) - startCol - 1
 
     for bigList in importantValues[1:]:
         dataDict = listToDict(bigList)
         currentDict = dataDict
         updateDict(outputDict, dataDict, depth)
         #print("result is:",outputDict,"\n")
-    dictToLists(outputDict, startCol, [], endList)
+    dictToLists(outputDict, borderIndex, [], endList)
     endList.sort(key = itemgetter(1,3,2,0))
     for row in endList:
         border = row[0]
