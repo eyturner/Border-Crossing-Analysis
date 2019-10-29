dd5e39452e4196349950743546a5a60caa57631b
Eli <eyturner@umich.edu>
2019-10-29 00:49:03 -0700

    Fixed typo

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 ./src/Border_analytics.py ./input/Border_Crossing_Entry_Data.csv ./output/report.csv
+python3 ./src/Border_Analytics.py ./input/Border_Crossing_Entry_Data.csv ./output/report.csv
